f9746a1d991e72d1f40e18da94aa14cd41c15678
Yunyu Lin <mail@yunyul.in>
2020-04-11 16:50:21 -0600

    AAA

--- src/js/background.js
+++ src/js/background.js
@@ -9,11 +9,37 @@ type ConnectionState = {
 */
 let connectionState = { status: "DISCONNECTED" };
 
+/*
+STRING { tabId?: number, windowId?: number }
+*/
+let currentActiveTab = "";
+const getTabKey = (tabInfo) => [tabInfo.tabId, tabInfo.windowId].join(",");
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([res]) => {
+  if (res) {
+    currentActiveTab = getTabKey({ tabId: res.id, windowId: res.windowId });
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  currentActiveTab = getTabKey(activeInfo);
+});
+
+const portsForTabs = new Map();
+
 chrome.runtime.onConnect.addListener((port) => {
   if (port.name === "popup") {
     port.postMessage(connectionState);
     port.onMessage.addListener((newConnectionState) => {
       connectionState = newConnectionState;
     });
+  } else if (port.name === "tab") {
+    const tabInfo = getTabKey({
+      tabId: port.sender.tab.id,
+      windowId: port.sender.tab.windowId,
+    });
+    if (tabInfo === currentActiveTab) {
+      console.log("received connect from active tab");
+    }
   }
 });
--- src/js/content.js
+++ src/js/content.js
@@ -2,3 +2,6 @@ import * as rrweb from "@yunyu/rrweb-patched";
 
 console.log("hello 3");
 console.log(rrweb);
+
+const port = chrome.runtime.connect({ name: "tab" });
+port.onMessage.addListener((msg) => console.log(msg));
