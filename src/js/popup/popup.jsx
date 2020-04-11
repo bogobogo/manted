6b4d164185ad65163e546fd9aa68e44c4ce0a8c6
Yunyu Lin <mail@yunyul.in>
2020-04-11 14:21:51 -0600

    Add storage permission

--- src/js/popup/popup.jsx
+++ src/js/popup/popup.jsx
@@ -3,12 +3,23 @@ import icon from "../../img/icon-128.png";
 import { hot } from "react-hot-loader";
 import cuid from "cuid";
 
-const HostView = ({ secret }) => {
-  return <div>Secret: {secret}</div>;
+const HostView = ({ roomName }) => {
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8082");
+
+    ws.addEventListener("open", () => {
+      ws.send(JSON.stringify({ type: "makeRoom", roomName }));
+    });
+
+    return () => ws.close();
+  }, []);
+
+  return <div>Hosting {roomName}</div>;
 };
 
 const App = () => {
-  const [recv, setRecv] = useState([]);
+  // undefined | { hosting: boolean, roomName: string }
+  const [room, setRoom] = useState(() => JSON.parse(chrome.storage.local));
 
   useEffect(() => {
     const listener = (request, sender, sendResponse) => {
@@ -24,22 +35,39 @@ const App = () => {
 
   return (
     <div style={{ padding: 24 }}>
-      <div
-        style={{
-          display: "flex",
-          width: "300px",
-          paddingBottom: "24px",
-          borderBottom: "1px solid #ccc",
-        }}
-      >
-        <input type="text" placeholder="Enter room code" />
-        <button style={{ flexShrink: 0, margin: 0, marginLeft: 8 }}>
-          Join {cuid.slug()}
-        </button>
-      </div>
-      <div style={{ paddingTop: "24px" }}>
-        <button style={{ margin: 0, width: "100%" }}>Make Room</button>
-      </div>
+      {room ? (
+        room.hosting ? (
+          <HostView roomName={room.roomName} />
+        ) : (
+          <h1>askdhjkoashdas</h1>
+        )
+      ) : (
+        <>
+          <div
+            style={{
+              display: "flex",
+              width: "300px",
+              paddingBottom: "24px",
+              borderBottom: "1px solid #ccc",
+            }}
+          >
+            <input type="text" placeholder="Enter room code" />
+            <button style={{ flexShrink: 0, margin: 0, marginLeft: 8 }}>
+              Join
+            </button>
+          </div>
+          <div style={{ paddingTop: "24px" }}>
+            <button
+              style={{ margin: 0, width: "100%" }}
+              onClick={() => {
+                setRoom({ hosting: true, roomName: cuid.slug() });
+              }}
+            >
+              Make Room
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
