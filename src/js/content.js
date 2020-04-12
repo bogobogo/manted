7f87fde9c81844305522fbf0937c6b771e67db9b
Yunyu Lin <mail@yunyul.in>
2020-04-11 18:08:02 -0600

    Use Lodash flush

--- src/js/content.js
+++ src/js/content.js
@@ -5,29 +5,31 @@ const port = chrome.runtime.connect({ name: "tab" });
 let stopFn = null;
 
 let queue = [];
-const flush = () => {
-  queue = [];
-};
-const debouncedFlush = debounce(flush, 50, {
-  leading: true,
-  maxWait: 100,
-});
+const debouncedFlush = debounce(
+  () => {
+    queue = [];
+  },
+  50,
+  {
+    leading: true,
+    maxWait: 100,
+  }
+);
 
 port.onMessage.addListener((msg) => {
+  console.log("Received action ", msg.action);
   if (msg.action === "startRecording") {
     stopFn = rrweb.record({
       emit(event, isCheckout) {
         queue.push([event, isCheckout]);
+        debouncedFlush();
         if (isCheckout !== undefined) {
-          flush();
-        } else {
-          debouncedFlush();
+          debouncedFlush.flush();
         }
       },
       checkoutEveryNth: 100,
     });
   } else if (msg.action === "stopRecording") {
-    console.log("Recording stopped");
     stopFn && stopFn();
   }
 });
